84890fc83232e4ff5866f5610ed5dde1ebac2956
Abdoulaye NDAO <ndao1301@gmail.com>
2026-03-30 11:44:19 +0000

    RAPPORT

--- Afrika_leyri.py
+++ Afrika_leyri.py
@@ -401,7 +401,7 @@ if menu == "AGREGATION":
         colonnne[1].metric("💴 CA RZ + TATA", f"{CA_donnee_RZ+donnee_ordre["Montant A verser"].sum():,.2f}".replace(",", " ")+" XOF")
         colonnne[0].metric("💴 CA TATA", f"{donnee_ordre["Montant A verser"].sum():,.2f}".replace(",", " ")+" XOF")
         colonnne[1].metric("🔢 Quantité vendue", f"{donnee_ordre["Quantités"].sum():,.2f}".replace(",", " "))
-        ca_restant=descente_T2["Montant"].sum()+descente_T1["Montant"].sum() #+descente_T3["Montant"].sum()
+        ca_restant=descente_T2["Montant"].sum()+descente_T1["Montant"].sum() +descente_T3["Montant"].sum()
         rest=stock_descente_T1.sum()+stock_descente_T2.sum()+stock_descente_T3.sum()
 
 
