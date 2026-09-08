b1576650fc4ca0348018b309719b86f1a699cc5a
Abdoulaye NDAO <ndao1301@gmail.com>
2026-09-08 12:28:22 +0000

    Fiche de paie

--- Afrika_leyri.py
+++ Afrika_leyri.py
@@ -662,7 +662,7 @@ elif menu == "FICHE":
         }, inplace=True)
 
         suivi["Salaire"] = suivi.apply(
-            lambda row: row["Jours travaillés"] * 5000
+            lambda row: row["Jours travaillés"] * 4000
             if row["Nom"].strip().upper() in ["DJIBRIL THIOMBANE (CHARRETIER)","ISSA KANE (CHARRETIER)","CHAMSDINE AIDARA (CHARRETIER)","MOHAMED DIONE (CHARRETIER)"]
             else row["Jours travaillés"] * 4000,
             axis=1
